84bd4311e57393189e802fd4bc7433b46d3d9d28
Cristian Buffa <bufface@gmail.com>
2016-07-11 13:47:49 -0300

    First Commit and Using our views title.

---
+++ simpleBlog/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var Bourne = require("bourne");
+
+var app = express();
+var posts = new Bourne("simpleBlogPosts.json");
+var comments = new Bourne("simpleBlogComments.json");
+
+app.configure(function(){
+ app.use(express.json());
+ app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get("/posts", function (req, res) {
+ posts.find(function (results) {
+   res.json(results);
+ });
+});
+
+app.post("/posts", function (req, res) {
+ posts.insert(req.body, function (result) {
+   res.json(result);
+ });
+});
+
+app.get('/*', function (req, res) {
+  posts.find(function (results) {
+    res.render("index.ejs", { posts: JSON.stringify(results) });
+  });
+});
+
+app.listen(3000);
